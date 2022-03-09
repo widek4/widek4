d8f8e35e1f8b7c2f196eeccbeb6fdee76c2b6aba
widek4 <51709313+widek4@users.noreply.github.com>
2022-03-09 17:15:55 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,14 +7,18 @@ let size = 0.7;
 let showHours = false;
 let dt = 0;
 let slider;
+let sliderSize;
 let checkbox;
 
 function setup() {
   colorMode(HSB);
   createCanvas(800, 800);
   slider = createSlider(1, 13, 10, 1);
+  sliderSize = createSlider(0,1,0.7,0.05);
+  sliderSize.position(10,25);
   slider.position(10, 10);
-  slider.style('width', '80px');
+  slider.style('width', '150px');
+  sliderSize.style("width", '150px');
   min = map(minute(),0,60,0,TWO_PI);
   checkbox = createCheckbox('Heures', false);
   //fullScreen();
@@ -27,6 +31,7 @@ function draw() {
     showHours = false;
   }
   depth = slider.value();
+  size=sliderSize.value();
   background(0);
   //fill(255);text(frameRate, 10, 10);noFill();
   translate(width/2, height/2);
@@ -43,6 +48,9 @@ function draw() {
   hours= map(hour()%12, 0, 12, 0, TWO_PI);
   drawAiguilles(0, 0, 0, scl, 0);
   dt+=0.2;
+  if (dt>360){
+    dt=0;
+  }
 }
 
 function drawAiguilles(iter,  x,  y,  scale,  phi) {
@@ -51,18 +59,18 @@ function drawAiguilles(iter,  x,  y,  scale,  phi) {
   }
   let sX = x + scale*cos(seconde+phi);
   let sY = y + scale*sin(seconde+phi);
-  let mX = x + 0.8*scale*cos(min+phi);
-  let mY = y + 0.8*scale*sin(min+phi);  
+  let mX = x + 0.75*scale*cos(min+phi);
+  let mY = y + 0.75*scale*sin(min+phi);  
   //stroke(map(iter,1,depth,255,100));
   strokeWeight(map(iter, 1, depth, 4,1));
   //strokeWeight(1);
-  stroke((15*iter+dt)%255, 255, map(iter, 0, depth, 255, 100), map(iter, 0, depth,220,75));
+  stroke((15*iter+dt)%360, 255, map(iter, 0, depth, 255, 100), map(iter, 0, depth,220,75));
   line(x, y, sX, sY);
   line(x, y, mX, mY);
 
   if (showHours) {
-    let hX = x + scale*0.6*cos(hours+phi);
-    let hY = y + scale*0.6*sin(hours+phi);
+    let hX = x + scale*0.5*cos(hours+phi);
+    let hY = y + scale*0.5*sin(hours+phi);
     line(x,y,hX,hY);
     drawAiguilles(iter+1, hX, hY, scale*size, hours+phi);
   }
